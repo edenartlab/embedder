1bd04f49403a614f113759a08c023a8a18d037d8
Gene Kogan <kogan.gene@gmail.com>
2023-07-23 17:13:44 -0400

    different clip

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /app
 
 ADD . /app
 
+RUN apt-get update && apt-get install -y git
+
 RUN pip install --no-cache-dir -r requirements.txt
 
 EXPOSE 80
--- main.py
+++ main.py
@@ -12,20 +12,22 @@ import numpy as np
 import zlib
 
 
-# Load the CLIP model
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#model, preprocess = torch.hub.load('openai/clip', 'clip400', device=device)
-model, preprocess = torch.hub.load('openai/clip', 'ViT-B/32', device=device)
 
-model.eval()
 
-print("test beginning 22")
 
-print("THIS IS MODEL", model)
+import torch
+import clip
+from PIL import Image
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+
+
+
 
 MONGO_URI = os.getenv('MONGO_URI')
 
-print("MONOGP yay", MONGO_URI)
+print("MONOGP yay open now", MONGO_URI)
 
 client = MongoClient(MONGO_URI)
 db = client['eden-dev']
@@ -40,23 +42,21 @@ while True:
         
         # Load and preprocess the image
         image_path = document['uri']
+        
+        
         image = Image.open(image_path).convert("RGB")
-        image = image.resize((224, 224), resample=Image.BICUBIC)
-        image = np.array(image) / 255.0
-        image = np.transpose(image, (2, 0, 1))
-        image = torch.from_numpy(image).unsqueeze(0).to(device)
 
-        # Generate the image embedding
+
+        image = preprocess(image).unsqueeze(0).to(device)
+
         with torch.no_grad():
             image_features = model.encode_image(image)
 
-        # Normalize the embedding
         image_features /= image_features.norm(dim=-1, keepdim=True)
-
-        # Convert the embedding to a numpy array
         embedding = image_features.cpu().numpy()
         
-        print("embedding", embedding)
+        print("embedding is here ", embedding)
+        print("the shape is", embedding.shape)
         
         time.sleep(1)
 
