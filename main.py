0d936c3ad7519313688774fd34313bbee675265a
Gene Kogan <kogan.gene@gmail.com>
2023-07-25 16:03:05 -0400

    try chroma?

--- main.py
+++ main.py
@@ -7,9 +7,56 @@ from io import BytesIO
 import numpy as np
 import torch
 import clip
+#dotenv
+from dotenv import load_dotenv
+load_dotenv()
 
+
+
+print("lets do chromadb")
+import chromadb
+chroma_client = chromadb.HttpClient(host="http://chromadb.mars", port=8000)
+print(chroma_client)
+print("great")
+
+
+collection = chroma_client.create_collection(name="test1")
+#collection = chroma_client.get_collection(name="test1")
+
+
+docs = [
+    {"doc": "cat", "metadata": {"tag": "animal"}, "id": "id1"},
+    {"doc": "dog", "metadata": {"tag": "animal"}, "id": "id2"},
+    {"doc": "pig", "metadata": {"tag": "animal"}, "id": "id3"},
+    {"doc": "blue", "metadata": {"tag": "color"}, "id": "id4"},
+    {"doc": "red", "metadata": {"tag": "color"}, "id": "id5"},
+    {"doc": "green", "metadata": {"tag": "color"}, "id": "id6"},
+    {"doc": "France", "metadata": {"tag": "country"}, "id": "id7"},
+    {"doc": "Germany", "metadata": {"tag": "country"}, "id": "id8"},    
+]
+
+
+collection.add(
+    documents=[doc['doc'] for doc in docs],
+    metadatas=[doc['metadata'] for doc in docs],
+    ids=[doc['id'] for doc in docs]
+)
+
+results = collection.query(
+    query_texts=["Brazil"],
+    n_results=3
+)
+
+print(results)
+
+
+print("now the rest....")
+
+print("GO!!!!")
 
 MONGO_URI = os.getenv('MONGO_URI')
+
+print("CONNECT", MONGO_URI)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
@@ -18,11 +65,28 @@ client = MongoClient(MONGO_URI)
 db = client['eden-dev']
 collection = db['creations']
 
+# docker build -t embedder .
+# docker run -p 27017:27017 -it embedder
+# 27017
+
+
+
+'''
+
+save to Embeddings collection
+_id
+creation
+embedding
+knn
+
+
+'''
+
 
 while True:
 
     for document in collection.find():
-        
+        print("new doc")
         try:
             print(document)
             
