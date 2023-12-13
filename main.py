7c42f8f47447f4ec07dc874f828b022824a16d36
Gene Kogan <kogan.gene@gmail.com>
2023-12-12 16:45:58 -0800

    simplify code, proper upsert

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("new version #7")
+print("new version #8")
 
 import sys
 sys.path.append('CLIP_assisted_data_labeling')
@@ -13,19 +13,17 @@ from PIL import Image
 from io import BytesIO
 import torch
 import chromadb
+from chromadb.config import Settings
 from utils.embedder import AestheticRegressor
 
-
-IN_DEV = True
 MONGO_URI = os.getenv('MONGO_URI')
 MONGO_DB_NAME = os.getenv('MONGO_DB_NAME')
 CHROMA_HOST = os.getenv('CHROMA_HOST')
 print("CHROMA HOST IS", CHROMA_HOST)
 
-
-model_path = "combo_2023-08-02_03:48:00_8.1k_imgs_80_epochs_-1.0000_mse.pth"
+#model_path = "combo_2023-08-02_03:48:00_8.1k_imgs_80_epochs_-1.0000_mse.pth"
+model_path = "eden_scorer_2023-12-13_9.4k_imgs_80_epochs_2_crops.pth"
 device = "cpu"
-generator_names = ["create", "remix", "blend", "upscale", "real2real", "interpolate", "wav2lip"]
 
 # setup mongo
 client = MongoClient(MONGO_URI)
@@ -33,14 +31,8 @@ db = client[MONGO_DB_NAME]
 creations = db['creations']
 generators = db['generators']
 
-
-
-import chromadb
-from chromadb.config import Settings
-
 # setup chroma
 try:
-    print("try auth chroma", CHROMA_HOST)
     chroma_client = chromadb.HttpClient(
         host=CHROMA_HOST, 
         port=8000,
@@ -49,11 +41,7 @@ try:
             chroma_client_auth_credentials="chromadb:changeme"
         )
     )
-
     print(chroma_client)
-
-    # chroma_client = chromadb.HttpClient(host=CHROMA_HOST, port=8000)
-    print("LETS GET CHROMA COLLECTION!!")
     collection = chroma_client.get_or_create_collection(name="creation_clip_embeddings")
     print(chroma_client.list_collections())
     print(f"Clip embeddings collection size: {collection.count()}")
@@ -61,53 +49,14 @@ except Exception as e:
     print(f"Failed to connect to chroma: {e}")
     print("Chroma is not available")
 
-# pre-populate chroma with all creations which already have embeddings
-# pipeline2 = [
-#     {
-#         "$match": {
-#             "embedding": {"$exists": True},
-#             "embedding.embedding": {"$exists": True},
-#             "embedding.score": {"$exists": True}
-#         }
-#     }
-# ]
-
-# pipeline = [
-#     {
-#         "$match": {
-#             "user": {"$exists": True}
-#         }
-#     },
-#     {
-#         "$limit": 50
-#     }
-# ]
-
-# print("e7")
-# documents = creations.aggregate(pipeline)
-# print("e9")
-# for document in documents:
-#     print(document)
-#     # collection.upsert(
-#     #     embeddings=[document['embedding']['embedding']],
-#     #     metadatas=[{"user": str(document['user'])}],
-#     #     ids=[str(document['_id'])]
-#     # )
-# print("e8")
-
-# print(f"Chroma now has {collection.count()} creations")
-
-# # load scorer + embedder
+# load scorer + embedder
 aesthetic_regressor = AestheticRegressor(model_path, device)
-
 print(aesthetic_regressor)
-
-
 
 
 def induct_creation(document):
     uri = document['thumbnail']
-    print("lets go", uri)
+
     if not uri:
         print(f"skip creation {document['_id']}, no thumbnail")
         return
@@ -132,31 +81,33 @@ def induct_creation(document):
         print(f"skip creation {document['_id']}, invalid score={score}")
         return
 
-    print("SET SCORE", score)
-    # update mongo
-    creations.update_one(
-        {'_id': document['_id']},
-        {
-            '$set': {
-                'embedding': {
-                    # 'embedding': embedding,
-                    'score': score
+    try:
+        # add to chroma
+        collection.upsert(
+            embeddings=[embedding],
+            metadatas=[{"user": str(document['user'])}],
+            ids=[str(document['_id'])]
+        )
+
+        # update mongo
+        creations.update_one(
+            {'_id': document['_id']},
+            {
+                '$set': {
+                    'embedding': {
+                        # 'embedding': embedding,
+                        'score': score
+                    }
                 }
             }
-        }
-    )
+        )
+        
+        print(f"inducted creation {document['_id']}")
+
+    except Exception as e:
+        print(f"error for creation {document['_id']}: {e}")
+        return
     
-    # add to chroma
-    collection.upsert(
-        embeddings=[embedding],
-        metadatas=[{"user": str(document['user'])}],
-        ids=[str(document['_id'])]
-    )
-
-    print(f"inducted creation {document['_id']}")
-
-
-
 
 def scan_unembedded_creations():
     query = {
@@ -187,7 +138,6 @@ def scan_unembedded_creations():
     cursor.close()
 
     print(f"Total number of creations scanned through: {processed_count}, inductions: {inductions}")
-
 
 
 while True:
